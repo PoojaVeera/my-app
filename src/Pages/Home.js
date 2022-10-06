d8dcac5937596e65bb736db4c8cb6cf1518a9f9c
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-10-06 19:11:22 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/Pages/Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import Card from "@material-ui/core/Card";
 export const Home = () => {
+  const users = [
+    { id: 1, name: "abc", email: "abc@abc.com" },
+    { id: 2, name: "cde", email: "cde@cde.com" },
+  ];
   return (
     <div>
       <h2>home </h2>
+      <label>Name:</label>&nbsp;&nbsp;
+      <input type="text"></input>
+      <br></br>
+      <label>Email:</label>&nbsp;&nbsp;
+      <input type="email"></input>
+      <br></br>
+      <button>Add user</button>
+      <Card>
+        {users.map((user) => (
+          <div>
+            Id: {user.id}&nbsp;&nbsp; Name: {user.name}&nbsp;&nbsp; Email:{" "}
+            {user.email}
+          </div>
+        ))}
+      </Card>
     </div>
   );
 };
