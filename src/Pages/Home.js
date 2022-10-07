ffffba7f90625fdc3ffd6cbd4dc61ab978913e8d
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-10-07 18:54:18 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/App.css
    	modified:   src/Pages/Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,39 +1,30 @@
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
 export const Home = () => {
-  const [name, setName] = useState([]);
-  const [mail, setMail] = useState([]);
-  const [users, setUsers] = [
+  const [name, setName] = useState();
+  const [mail, setMail] = useState();
+  const users = [
     { id: 1, name: "abc", email: "abc@abc.com" },
     { id: 2, name: "cde", email: "cde@cde.com" },
   ];
+
   const handleAdd = (e) => {
     e.preventDefault();
     let len = users.length;
     let Id = len + 1;
     let a = name,
       b = mail;
-    let newUsers = [...users];
-    newUsers.push({ id: Id, name: a, mail: b });
+    users.push({ id: Id, name: a, mail: b });
     console.log(users);
-    setUsers(newUsers);
     alert("added");
     clearState();
   };
   const clearState = () => {
     setName("");
     setMail("");
+    alert("cleared ");
   };
-  const data = [users].map((user) => (
-    <div>
-      <Card>
-        Id: {user.id}
-        <br /> Name: {user.name}
-        <br /> Email: {user.email}
-      </Card>
-      <br></br>
-    </div>
-  ));
+
   return (
     <div>
       <h2>home </h2>
@@ -54,6 +45,18 @@ export const Home = () => {
       <button type="submit" onClick={handleAdd}>
         Add user
       </button>
+      {users.map((user) => {
+        return (
+          <div>
+            <Card>
+              Id: {user.id}
+              <br /> Name: {user.name}
+              <br /> Email: {user.email}
+            </Card>
+            <br></br>
+          </div>
+        );
+      })}
     </div>
   );
 };
